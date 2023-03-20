2079d19de0cb2e40630fd038a71459e4f13b3a09
c0wsaysmoo <127139588+c0wsaysmoo@users.noreply.github.com>
2023-03-20 17:56:15 -0500

    Add files via upload

---
+++ its-a-plane-python/config.py
@@ -0,0 +1,20 @@
+ZONE_HOME = {
+    "tl_y": xxx, # Top-Left Latitude (deg)
+    "tl_x": xxx, # Top-Left Longitude (deg)
+    "br_y": xxx, # Bottom-Right Latitude (deg)
+    "br_x": xxx, # Bottom-Right Longitude (deg)
+}
+LOCATION_HOME = [
+    xxx, # Latitude (deg)
+    xxx, # Longitude (deg)
+    xxx # Altitude (6371+home altitude (km))
+]
+TEMPERATURE_LOCATION = "zip code,country code"
+OPENWEATHER_API_KEY = "xxx" # Get an API key from https://openweathermap.org/price
+TEMPERATURE_UNITS = "imperial"
+MIN_ALTITUDE = 2600
+BRIGHTNESS = 75
+GPIO_SLOWDOWN = 5
+JOURNEY_CODE_SELECTED = "xxx"
+JOURNEY_BLANK_FILLER = " ? "
+HAT_PWM_ENABLED = True #false if no soldering
