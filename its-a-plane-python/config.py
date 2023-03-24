21c809f5c3cde98a2568431b57c069f2477e2a74
c0wsaysmoo <127139588+c0wsaysmoo@users.noreply.github.com>
2023-03-23 22:56:29 -0500

    Update config.py

--- its-a-plane-python/config.py
+++ its-a-plane-python/config.py
@@ -1,21 +1,21 @@
 ZONE_HOME = {
-    "tl_y": 41.953371, # Top-Left Latitude (deg)
-    "tl_x": -87.746000, # Top-Left Longitude (deg)
-    "br_y": 41.889507, # Bottom-Right Latitude (deg)
-    "br_x": -87.623695 # Bottom-Right Longitude (deg)
+    "tl_y": xx.xxx, # Top-Left Latitude (deg)
+    "tl_x": xx.xxx, # Top-Left Longitude (deg)
+    "br_y": xx.xxx, # Bottom-Right Latitude (deg)
+    "br_x": xx.xxx # Bottom-Right Longitude (deg)
 }
 LOCATION_HOME = [
-    41.926550, # Latitude (deg)
-    -87.695440, # Longitude (deg)
-    6371.181 # Altitude (6371+home altitude)
+    xx.xxx, # Latitude (deg)
+    xx.xxx, # Longitude (deg)
+    6371.xxx # Altitude (6371+home altitude)
 ]
-TEMPERATURE_LOCATION = "41.926550,-87.695440"
-WHEATHER_API_API_KEY = "b29b3fa213854fd38ac121912232303" # Get an API key from https://weatherapi.com
+TEMPERATURE_LOCATION = "xxx" #lat,long or your place or city or zip code
+WHEATHER_API_API_KEY = "xxxxxxx" # Get an API key from https://www.weatherapi.com/
 TEMPERATURE_UNITS = "imperial"
 MIN_ALTITUDE = 2600
 BRIGHTNESS = 75
 GPIO_SLOWDOWN = 2
-JOURNEY_CODE_SELECTED = "ORD"
+JOURNEY_CODE_SELECTED = "xxx"
 JOURNEY_BLANK_FILLER = " ? "
 HAT_PWM_ENABLED = True
 FORECAST_DAYS = "3"
