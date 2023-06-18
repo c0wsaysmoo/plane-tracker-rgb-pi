66627d58fde9621e11e8deb6d57dad626f87e143
c0wsaysmoo <127139588+c0wsaysmoo@users.noreply.github.com>
2023-06-18 16:20:34 -0500

    Update temperature.py

--- its-a-plane-python/utilities/temperature.py
+++ its-a-plane-python/utilities/temperature.py
@@ -18,7 +18,7 @@ if TEMPERATURE_UNITS == "metric":
     TEMPERATURE_MAX = 25
 elif TEMPERATURE_UNITS == "imperial":
     TEMPERATURE_MIN = 32
-    TEMPERATURE_MAX = 90
+    TEMPERATURE_MAX = 100
 
 if TEMPERATURE_UNITS != "metric" and TEMPERATURE_UNITS != "imperial":
     TEMPERATURE_UNITS = "metric"
