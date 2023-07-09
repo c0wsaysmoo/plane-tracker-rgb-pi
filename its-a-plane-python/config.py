6b499d4647183fef1ea3d656ef27e2cfa069bc51
c0wsaysmoo <127139588+c0wsaysmoo@users.noreply.github.com>
2023-07-09 13:06:48 -0500

    Update config.py

--- its-a-plane-python/config.py
+++ its-a-plane-python/config.py
@@ -7,15 +7,15 @@ ZONE_HOME = {
 LOCATION_HOME = [
     xx.xxx, # Latitude (deg)
     xx.xxx, # Longitude (deg)
-    6371.xxx # Altitude (6371+home altitude)
+    6371.xxx # Altitude (6371+home altitude km)
 ]
-TEMPERATURE_LOCATION = "xxx" #lat,long or your place or city or zip code
-WHEATHER_API_API_KEY = "xxxxxxx" # Get an API key from https://www.weatherapi.com/
+TEMPERATURE_LOCATION = "xx.xxx,xx,xxx" #lat,long of your place usually the same as LOCATION_HOME
+WHEATHER_API_API_KEY = "xxxxxxx" # Get an API key from https://www.tomorrow.io/
 TEMPERATURE_UNITS = "imperial"
-MIN_ALTITUDE = 2600
+MIN_ALTITUDE = 2600 #feet
 BRIGHTNESS = 75
 GPIO_SLOWDOWN = 2
 JOURNEY_CODE_SELECTED = "xxx" # the airport code of where you live
 JOURNEY_BLANK_FILLER = " ? "
-HAT_PWM_ENABLED = True
+HAT_PWM_ENABLED = True #if you didn't solder type False
 FORECAST_DAYS = "3"
