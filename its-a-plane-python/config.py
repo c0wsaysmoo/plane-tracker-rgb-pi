42137ef4187c291446c970cbc50ac91eee9a2d81
c0wsaysmoo <127139588+c0wsaysmoo@users.noreply.github.com>
2023-07-10 21:29:36 -0500

    Update config.py

--- its-a-plane-python/config.py
+++ its-a-plane-python/config.py
@@ -11,7 +11,7 @@ LOCATION_HOME = [
 ]
 TEMPERATURE_LOCATION = "xx.xxx,xx.xxx" #lat,long of your place usually the same as 
 TOMORROW_API_KEY = "xxxxxxx" # Get an API key from https://www.tomorrow.io/
-TEMPERATURE_UNITS = "imperial"
+TEMPERATURE_UNITS = "imperial" #type metric if you want that instead for whatever reason
 MIN_ALTITUDE = 2600 #feet
 BRIGHTNESS = 100
 GPIO_SLOWDOWN = 2
