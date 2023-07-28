9d034b0c987132e7748e3f9b4b3163c209a7cffa
c0wsaysmoo <127139588+c0wsaysmoo@users.noreply.github.com>
2023-07-28 06:38:03 -0500

    Add files via upload

--- its-a-plane-python/scenes/temperature.py
+++ its-a-plane-python/scenes/temperature.py
@@ -1,18 +1,15 @@
 from datetime import datetime
+import colorsys
 from rgbmatrix import graphics
 from utilities.animator import Animator
 from setup import colours, fonts, frames
-
-from utilities.temperature import grab_temperature, TEMPERATURE_MIN, TEMPERATURE_MAX
-
+from utilities.temperature import grab_temperature_and_humidity
 
 # Scene Setup
 TEMPERATURE_REFRESH_SECONDS = 600
 TEMPERATURE_FONT = fonts.small
 TEMPERATURE_FONT_HEIGHT = 6
 TEMPERATURE_POSITION = (42, TEMPERATURE_FONT_HEIGHT)
-TEMPERATURE_MIN_COLOUR = colours.BLUE
-TEMPERATURE_MAX_COLOUR = colours.ORANGE
 
 
 class TemperatureScene(object):
@@ -22,6 +19,7 @@ class TemperatureScene(object):
         self._last_temperature_str = None
         self._last_updated = None
         self._cached_temp = None
+        self._cached_humidity = None
         self._redraw_temp = True
 
     def colour_gradient(self, colour_A, colour_B, ratio):
@@ -29,7 +27,9 @@ class TemperatureScene(object):
             colour_A.red + ((colour_B.red - colour_A.red) * ratio),
             colour_A.green + ((colour_B.green - colour_A.green) * ratio),
             colour_A.blue + ((colour_B.blue - colour_A.blue) * ratio),
-        )
+            )
+
+        return graphics.Color(int(r), int(g), int(b))
 
     @Animator.KeyFrame.add(frames.PER_SECOND * 1)
     def temperature(self, count):
@@ -41,11 +41,11 @@ class TemperatureScene(object):
         seconds_since_update = (datetime.now() - self._last_updated).seconds if self._last_updated is not None else 0
         if not (seconds_since_update % TEMPERATURE_REFRESH_SECONDS) or self._redraw_temp:
             if self._cached_temp is not None and self._redraw_temp:
-                current_temperature = self._cached_temp
+                current_temperature, current_humidity = self._cached_temp
             else:
-                self._cached_temp = current_temperature = grab_temperature()
+                self._cached_temp = current_temperature, current_humidity = grab_temperature_and_humidity()
                 self._last_updated = datetime.now()
-            
+
             # Undraw old temperature
             if self._last_temperature_str is not None:
                 _ = graphics.DrawText(
@@ -62,16 +62,10 @@ class TemperatureScene(object):
                 self._last_temperature = current_temperature
                 self._redraw_temp = False
 
-                if current_temperature > TEMPERATURE_MAX:
-                    ratio = 1
-                elif current_temperature > TEMPERATURE_MIN:
-                    ratio = (current_temperature - TEMPERATURE_MIN) / TEMPERATURE_MAX
-                else:
-                    ratio = 0
+                # Get the humidity ratio (0% -> white, 100% -> blue)
+                humidity_ratio = current_humidity / 100.0
 
-                temp_colour = self.colour_gradient(
-                    TEMPERATURE_MIN_COLOUR, TEMPERATURE_MAX_COLOUR, ratio
-                )
+                temp_colour = self.colour_gradient(colours.WHITE, colours.BLUE, humidity_ratio)
 
                 # Draw temperature
                 _ = graphics.DrawText(
