8cbbae318286c1c0be5e982553c5d0087b4c62a9
c0wsaysmoo <127139588+c0wsaysmoo@users.noreply.github.com>
2023-08-20 18:20:53 -0500

    Update config.py

--- its-a-plane-python/config.py
+++ its-a-plane-python/config.py
@@ -1,15 +1,15 @@
 ZONE_HOME = {
-    "tl_y": xx.xxx, # Top-Left Latitude (deg)
+    "tl_y": xx.xxx, # Top-Left Latitude (deg) https://www.latlong.net/
     "tl_x": xx.xxx, # Top-Left Longitude (deg)
     "br_y": xx.xxx, # Bottom-Right Latitude (deg)
     "br_x": xx.xxx # Bottom-Right Longitude (deg)
 }
 LOCATION_HOME = [
-    xx.xxx, # Latitude (deg)
+    xx.xxx, # Latitude (deg) https://www.latlong.net/
     xx.xxx, # Longitude (deg)
-    3958.8.xxx # Altitude (3958.8+home altitude mi)
+    xxx # Altitude (ft) https://whatismyelevation.com/
 ]
-TEMPERATURE_LOCATION = "xx.xxx,xx.xxx" #lat,long of your place usually the same as 
+TEMPERATURE_LOCATION = "xx.xxx,xx.xxx" #lat,long of your place usually the same as location_home
 TOMORROW_API_KEY = "xxxxxxx" # Get an API key from https://www.tomorrow.io/
 TEMPERATURE_UNITS = "imperial" #type metric if you want that instead for whatever reason
 DISTANCE_UNITS = "imperial" #type metric if you want that instead for whatever reason
