ae937cfca1804d3784810620ab4e798f8b156468
c0wsaysmoo <127139588+c0wsaysmoo@users.noreply.github.com>
2024-04-14 09:22:03 -0700

    Add files via upload

--- its-a-plane-python/display/__init__.py
+++ its-a-plane-python/display/__init__.py
@@ -1,5 +1,5 @@
 import sys
-
+from datetime import datetime
 from setup import frames
 from utilities.animator import Animator
 from utilities.overhead import Overhead
@@ -28,14 +28,54 @@ def flight_updated(flights_a, flights_b):
 
 try:
     # Attempt to load config data
-    from config import BRIGHTNESS, GPIO_SLOWDOWN, HAT_PWM_ENABLED
+    from config import (
+        BRIGHTNESS,
+        GPIO_SLOWDOWN,
+        HAT_PWM_ENABLED,
+        BRIGHTNESS_NIGHT,
+        NIGHT_START,
+        NIGHT_END,
+        NIGHT_BRIGHTNESS,
+    )
+    # Parse NIGHT_START and NIGHT_END from strings to datetime objects
+    NIGHT_START = datetime.strptime(NIGHT_START, "%H:%M")
+    NIGHT_END = datetime.strptime(NIGHT_END, "%H:%M")
 
 except (ModuleNotFoundError, NameError):
     # If there's no config data
     BRIGHTNESS = 100
     GPIO_SLOWDOWN = 1
     HAT_PWM_ENABLED = True
+    NIGHT_BRIGHTNESS = False
 
+def adjust_brightness(matrix, display):
+    if NIGHT_BRIGHTNESS is False:
+        return  # Do nothing if NIGHT_BRIGHTNESS is False
+        
+    # Redraw screen every frame
+    now = datetime.now().time().replace(second=0, microsecond=0)  # Extract only hours and minutes
+    night_start_time = NIGHT_START.time().replace(second=0, microsecond=0)
+    night_end_time = NIGHT_END.time().replace(second=0, microsecond=0)
+
+    #print("Current time (HH:MM):", now)
+    #print("NIGHT_START (HH:MM):", night_start_time)
+    #print("NIGHT_END (HH:MM):", night_end_time)
+
+    # Check if current time is after NIGHT_END and before NIGHT_START
+    if night_end_time <= now < night_start_time:
+        new_brightness = BRIGHTNESS
+    else:
+        new_brightness = BRIGHTNESS_NIGHT
+        
+    # Check if the brightness has changed
+    if matrix.brightness != new_brightness:
+        # Update the brightness
+        matrix.brightness = new_brightness
+
+        # Redraw the screen with new data
+        display.redraw_with_current_brightness()
+
+    #print("Brightness:", matrix.brightness)
 
 class Display(
     TemperatureScene,
@@ -92,6 +132,41 @@ class Display(
     def draw_square(self, x0, y0, x1, y1, colour):
         for x in range(x0, x1):
             _ = graphics.DrawLine(self.canvas, x, y0, x, y1, colour)
+            
+    def redraw_with_current_brightness(self):
+        """
+        Redraws the screen with the current brightness level.
+        """
+        # Redraw the screen with new data
+        self.clear_screen()  # Clear the screen
+        self.sync()  # Synchronize the display
+            
+    def check_for_loaded_data(self, count=None):
+        if self.overhead.new_data:
+            # Check if there's data
+            there_is_data = len(self._data) > 0 or not self.overhead.data_is_empty
+
+            # this marks self.overhead.data as no longer new
+            new_data = self.overhead.data
+
+            # See if this matches the data already on the screen
+            # This test only checks if it's 2 lists with the same
+            # callsigns, regardless or order
+            data_is_different = not flight_updated(self._data, new_data)
+
+            if data_is_different:
+                self._data_index = 0
+                self._data_all_looped = False
+                self._data = new_data
+
+            # Only reset if there's flight data already
+            # on the screen, of if there's some new
+            # data available to draw which is different
+            # from the current data
+            reset_required = there_is_data and data_is_different
+
+            if reset_required:
+                self.reset_scene()
 
     @Animator.KeyFrame.add(0)
     def clear_screen(self):
@@ -128,9 +203,12 @@ class Display(
                 self.reset_scene()
 
     @Animator.KeyFrame.add(1)
-    def sync(self, count):
+    def sync(self, count=None):
         # Redraw screen every frame
         _ = self.matrix.SwapOnVSync(self.canvas)
+
+        # Adjust brightness
+        adjust_brightness(self.matrix, self)
 
     @Animator.KeyFrame.add(frames.PER_SECOND * 30)
     def grab_new_data(self, count):
@@ -152,8 +230,15 @@ class Display(
         try:
             # Start loop
             print("Press CTRL-C to stop")
-            self.play()
+            while True:
+                self.redraw_with_current_brightness()
+                self.grab_new_data(0)  # Call grab_new_data without waiting
+                self.play()  # Continue with scene animations
 
         except KeyboardInterrupt:
             print("Exiting\n")
             sys.exit(0)
+            
+if __name__ == "__main__":
+    display = Display()
+    display.run()
