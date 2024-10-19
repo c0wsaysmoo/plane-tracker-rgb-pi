a4d2ac3a306be3005ae25819f930053ca351fe38
c0wsaysmoo <127139588+c0wsaysmoo@users.noreply.github.com>
2024-10-19 13:56:30 -0500

    Update config.py

--- its-a-plane-python/config.py
+++ its-a-plane-python/config.py
@@ -8,7 +8,7 @@ LOCATION_HOME = [
     xx.xxxxxx, # Latitude (deg)
     xx.xxxxxx # Longitude (deg)
 ]
-TEMPERATURE_LOCATION = "xx.xxxxxx,xx.xxxxxx"
+TEMPERATURE_LOCATION = "xx.xxxxxx,xx.xxxxxx" #same as location home
 TOMORROW_API_KEY = "xxxxxxx" # Get an API key from https://tomorrow.io
 TEMPERATURE_UNITS = "imperial"
 DISTANCE_UNITS = "imperial"
@@ -17,10 +17,10 @@ MIN_ALTITUDE = 2600 #feet
 BRIGHTNESS = 100
 BRIGHTNESS_NIGHT = 50
 NIGHT_BRIGHTNESS = False #True for on False for off
-NIGHT_START = "22:00"
+NIGHT_START = "22:00" #dims screen between these hours
 NIGHT_END = "06:00"
 GPIO_SLOWDOWN = 2 #depends what Pi you have I use 2 for Pi 3 and 1 for Pi Zero
-JOURNEY_CODE_SELECTED = "xxx"
-JOURNEY_BLANK_FILLER = " ? "
+JOURNEY_CODE_SELECTED = "xxx" #bolds your airport code ie PDX
+JOURNEY_BLANK_FILLER = " ? " #what to display if theres no airport code
 HAT_PWM_ENABLED = True
 FORECAST_DAYS = 3 #today plus the next two days
