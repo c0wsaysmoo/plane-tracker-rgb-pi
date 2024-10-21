d684855169a68da0c3ebec0b278619093c1676fc
c0wsaysmoo <127139588+c0wsaysmoo@users.noreply.github.com>
2024-10-21 16:28:55 -0500

    Update config.py

--- its-a-plane-python/config.py
+++ its-a-plane-python/config.py
@@ -1,5 +1,5 @@
 ZONE_HOME = {
-    "tl_y": xx.xxxxxx, # Top-Left Latitude (deg) https://www.latlong.net/
+    "tl_y": xx.xxxxxx, # Top-Left Latitude (deg) https://www.latlong.net/ the bigger the zone, the more planes you'll get
     "tl_x": xx.xxxxxx, # Top-Left Longitude (deg)
     "br_y": xx.xxxxxx, # Bottom-Right Latitude (deg)
     "br_x": xx.xxxxxx # Bottom-Right Longitude (deg)
@@ -9,7 +9,7 @@ LOCATION_HOME = [
     xx.xxxxxx # Longitude (deg)
 ]
 TEMPERATURE_LOCATION = "xx.xxxxxx,xx.xxxxxx" #same as location home
-TOMORROW_API_KEY = "xxxxxxx" # Get an API key from https://tomorrow.io
+TOMORROW_API_KEY = "xxxxxxx" # Get an API key from https://tomorrow.io they only allows 25 pulls an hour, if you reach the limit you'll need to wait until the next hour 
 TEMPERATURE_UNITS = "imperial"
 DISTANCE_UNITS = "imperial"
 CLOCK_FORMAT = "12hr" #use 12hr or 24hr
@@ -20,7 +20,7 @@ NIGHT_BRIGHTNESS = False #True for on False for off
 NIGHT_START = "22:00" #dims screen between these hours
 NIGHT_END = "06:00"
 GPIO_SLOWDOWN = 2 #depends what Pi you have I use 2 for Pi 3 and 1 for Pi Zero
-JOURNEY_CODE_SELECTED = "xxx" #bolds your airport code ie PDX
+JOURNEY_CODE_SELECTED = "xxx" #your home airport code
 JOURNEY_BLANK_FILLER = " ? " #what to display if theres no airport code
-HAT_PWM_ENABLED = True
+HAT_PWM_ENABLED = True #only if you have soldered the PWM bridge use False if you didn't
 FORECAST_DAYS = 3 #today plus the next two days
