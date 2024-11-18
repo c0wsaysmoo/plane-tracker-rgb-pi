48ab11a9b553895108628db18b1ca2d91794a702
c0wsaysmoo <127139588+c0wsaysmoo@users.noreply.github.com>
2024-11-18 07:21:17 -0600

    Update colours.py

--- its-a-plane-python/setup/colours.py
+++ its-a-plane-python/setup/colours.py
@@ -1,71 +1,75 @@
 from rgbmatrix import graphics
 
-# Colour helpers
-BLACK = graphics.Color(0, 0, 0)
+# Colour helpers from lightest to darkest
+
 WHITE = graphics.Color(255, 255, 255)
 GREY = graphics.Color(192, 192, 192)
+LIGHT_GREY = graphics.Color(188, 189, 188)
+LIGHT_DARK_GREY = graphics.Color(121, 145, 156)
+BLACK = graphics.Color(0, 0, 0)
 
 # Shades of Yellow
 YELLOW = graphics.Color(255, 255, 0)
+LIGHT_YELLOW = graphics.Color(254, 239, 88)
+TROPICAL_YELLOW = graphics.Color(245, 190, 76)
 YELLOW_DARK = graphics.Color(128, 128, 0)
 
 # Shades of Blue
-BLUE = graphics.Color(0, 0, 255)
-BLUE_MEDIUM = graphics.Color(0, 102, 204)
+LIGHT_BLUE = graphics.Color(40, 183, 246)
+LIGHT_MID_BLUE = graphics.Color(66, 164, 244)
 BLUE_LIGHT = graphics.Color(110, 182, 255)
-BLUE_DARK = graphics.Color(29, 0, 156)
-CERULEAN_BLUE = graphics.Color(42, 82, 190)
+LIGHT_DARK_BLUE = graphics.Color(92, 107, 192)
+BLUE_MEDIUM = graphics.Color(0, 102, 204)
 TROPICAL_LIGHT_BLUE = graphics.Color(102, 199, 221)
 TROPICAL_BLUE = graphics.Color(107, 170, 182)
 TROPICAL_DARK_BLUE = graphics.Color(62, 130, 135)
-LIGHT_DARK_BLUE = graphics.Color(92, 107, 192)
-LIGHT_MID_BLUE = graphics.Color(66, 164, 244)
-LIGHT_BLUE = graphics.Color(40, 183, 246)
+CERULEAN_BLUE = graphics.Color(42, 82, 190)
+BLUE = graphics.Color(0, 0, 255)
+BLUE_DARK = graphics.Color(29, 0, 156)
 DARK_BLUE = graphics.Color(12, 70, 230)
 
 # Shades of Pink and Purple
-PINK = graphics.Color(200, 0, 200)
-PINK_DARK = graphics.Color(112, 0, 145)
-PINK_DARKER = graphics.Color(96, 1, 125)
 LIGHT_PINK = graphics.Color(236, 65, 123)
-PURPLE = graphics.Color(128, 0, 128)
-MIDDLE_PURPLE = graphics.Color(184, 128, 200)
+PINK = graphics.Color(200, 0, 200)
 LIGHT_PURPLE = graphics.Color(170, 70, 188)
-DARK_PURPLE = graphics.Color(143, 37, 170)
+MIDDLE_PURPLE = graphics.Color(184, 128, 200)
 DARK_MID_PURPLE = graphics.Color(199, 146, 212)
 LIGHT_DARK_PURPLE = graphics.Color(126, 86, 195)
+PINK_DARK = graphics.Color(112, 0, 145)
+PINK_DARKER = graphics.Color(96, 1, 125)
+PURPLE = graphics.Color(128, 0, 128)
+DARK_PURPLE = graphics.Color(143, 37, 170)
 
 # Shades of Green
-GREEN = graphics.Color(0, 255, 0)
 LIMEGREEN = graphics.Color(50, 205, 50)
-TROPICAL_GREEN = graphics.Color(69, 160, 123)
+LIGHT_LIGHT_GREEN = graphics.Color(128, 198, 132)
 LIGHT_MID_GREEN = graphics.Color(103, 186, 106)
 LIGHT_GREEN = graphics.Color(156, 204, 101)
-LIGHT_LIGHT_GREEN = graphics.Color(128, 198, 132)
 LIGHT_DARK_GREEN = graphics.Color(39, 166, 155)
+GREEN = graphics.Color(0, 255, 0)
+TROPICAL_GREEN = graphics.Color(69, 160, 123)
 
 # Shades of Orange
-ORANGE = graphics.Color(227, 110, 0)
-ORANGE_DARK = graphics.Color(113, 55, 0)
-TROPICAL_ORANGE = graphics.Color(226, 141, 59)
 LIGHT_ORANGE = graphics.Color(254, 167, 39)
 LIGHT_MID_ORANGE = graphics.Color(255, 141, 53)
+TROPICAL_ORANGE = graphics.Color(226, 141, 59)
 LIGHT_DARK_ORANGE = graphics.Color(255, 113, 66)
+ORANGE = graphics.Color(227, 110, 0)
+ORANGE_DARK = graphics.Color(113, 55, 0)
 
 # Shades of Red
-RED = graphics.Color(255, 0, 0)
 RED_LIGHT = graphics.Color(255, 195, 195)
 LIGHT_LIGHT_RED = graphics.Color(228, 115, 114)
 LIGHT_RED = graphics.Color(239, 82, 81)
+RED = graphics.Color(255, 0, 0)
 
 # Other Colors
+MELTWATER = graphics.Color(185, 235, 255)
+TROPICAL_PINK = graphics.Color(217, 109, 126)
+CYAN = graphics.Color(0, 255, 255)
 TEAL = graphics.Color(0, 165, 165)
 LIGHT_TEAL = graphics.Color(38, 198, 218)
 LIGHT_DARK_TEAL = graphics.Color(39, 166, 155)
-CYAN = graphics.Color(0, 255, 255)
-MELTWATER = graphics.Color(185, 235, 255)
-TROPICAL_YELLOW = graphics.Color(245, 190, 76)
-TROPICAL_PINK = graphics.Color(217, 109, 126)
-LIGHT_DARK_GREY = graphics.Color(121, 145, 156)
-LIGHT_GREY = graphics.Color(188, 189, 188)
+
+
 
