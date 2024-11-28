72f2aa84fdf1f8a1d151ce941db71f0541a9e970
c0wsaysmoo <127139588+c0wsaysmoo@users.noreply.github.com>
2024-11-28 08:17:09 -0600

    Update config.py

--- its-a-plane-python/config.py
+++ its-a-plane-python/config.py
@@ -1,5 +1,5 @@
 ZONE_HOME = {
-    "tl_y": xx.xxxxxx, # Top-Left Latitude (deg) https://www.latlong.net/ the bigger the zone, the more planes you'll get
+    "tl_y": xx.xxxxxx, # Top-Left Latitude (deg) https://www.latlong.net/ the bigger the zone, the more planes you'll get. My zone is ~3.5 miles in each direction or 10mi corner to corner. 
     "tl_x": xx.xxxxxx, # Top-Left Longitude (deg)
     "br_y": xx.xxxxxx, # Bottom-Right Latitude (deg)
     "br_x": xx.xxxxxx # Bottom-Right Longitude (deg)
@@ -13,7 +13,7 @@ TOMORROW_API_KEY = "xxxxxxx" # Get an API key from https://tomorrow.io they only
 TEMPERATURE_UNITS = "imperial" #can use "metric" if you want, same for distance 
 DISTANCE_UNITS = "imperial"
 CLOCK_FORMAT = "12hr" #use 12hr or 24hr
-MIN_ALTITUDE = 2600 #feet
+MIN_ALTITUDE = 2600 #feet above sea level. If you live at 1000ft then you'd want to make yours ~3600 etc. I use 2600 to weed out some of the smaller general aviation traffic. 
 BRIGHTNESS = 100
 BRIGHTNESS_NIGHT = 50
 NIGHT_BRIGHTNESS = False #True for on False for off
@@ -22,5 +22,5 @@ NIGHT_END = "06:00"
 GPIO_SLOWDOWN = 2 #depends what Pi you have I use 2 for Pi 3 and 1 for Pi Zero
 JOURNEY_CODE_SELECTED = "xxx" #your home airport code
 JOURNEY_BLANK_FILLER = " ? " #what to display if theres no airport code
-HAT_PWM_ENABLED = True #only if you have soldered the PWM bridge use False if you didn't
+HAT_PWM_ENABLED = False #only if you haven't soldered the PWM bridge use True if you did
 FORECAST_DAYS = 3 #today plus the next two days
