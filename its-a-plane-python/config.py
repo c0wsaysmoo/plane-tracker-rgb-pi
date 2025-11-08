b2ccf277ffd23972f90e104bbf6f9c0d6fc439da
c0wsaysmoo <127139588+c0wsaysmoo@users.noreply.github.com>
2025-11-08 11:33:53 -0600

    Update config.py

--- its-a-plane-python/config.py
+++ its-a-plane-python/config.py
@@ -25,4 +25,4 @@ JOURNEY_BLANK_FILLER = " ? " #what to display if theres no airport code
 HAT_PWM_ENABLED = False #only if you haven't soldered the PWM bridge use True if you did
 FORECAST_DAYS = 3 #today plus the next two days
 EMAIL = "" #insert your email address between the " ie "example@example.com" to recieve emails when there is a new closest flight on the tracker. Leave "" to recieve no emails
-
+MAX_FARTHEST = 3 #the amount of furthest flights you want in your log
