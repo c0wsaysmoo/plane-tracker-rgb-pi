b550daaa34f3d867d0ace07053574e986e659067
a10kiloham <295361+a10kiloham@users.noreply.github.com>
2026-05-08 22:07:21 +0100

    privilege crap

--- its-a-plane-python/display/__init__.py
+++ its-a-plane-python/display/__init__.py
@@ -100,7 +100,7 @@ class Display(
         options.show_refresh_rate = 0
         options.gpio_slowdown = GPIO_SLOWDOWN
         options.disable_hardware_pulsing = True
-        options.drop_privileges = True
+        options.drop_privileges = False
         options.limit_refresh_rate_hz = 120
         self.matrix = RGBMatrix(options=options)
 
